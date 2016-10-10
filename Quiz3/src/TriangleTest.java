e4919e7540ce62ad20068976082ca5447a091a6a
josephportale <josephportale@wifi-roaming-128-4-166-46.wifi.udel.edu>
2016-10-10 03:28:56 -0400

    Quiz3 Commit

---
+++ Quiz3/src/TriangleTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TriangleTest {
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testArea() {
+		Triangle myTriangle = new Triangle(10,10,10);
+		assertEquals(43.3,43.3);
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void testPerimeter() {
+		Triangle myTriangle = new Triangle(4,6,2);
+		assertEquals(12.0,12.0);
+		}
+	}
+
